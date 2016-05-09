bb20e96fff4ac0b23adf281a912bce95d9b7f510
ecuSinner <deivydas.ziziliauskas@gmail.com>
2016-05-09 20:24:04 +0300

    Fixing parse, from array to hash

--- raides.rb
+++ raides.rb
@@ -1,33 +1,21 @@
-#require 'pry'
 require 'json'
 
 def read(file_name)
   file = File.open(file_name, 'r')
   text = file.read
-  text = text.chomp.downcase  # split(" ".sort
+  text = text.chomp.downcase 
    text.gsub!(/\W+/, '')
    text.gsub!(/\d+/, '')
   hsh = Hash.new(0)
   text.each_line do |line|
     line.split.each do |word|
       word.split('').each do |letter|
-    hsh[letter] += 1
-   
+   hsh[letter] += 1
     end
   end
-
 end
-
-#puts hsh.count
-
-#puts hsh
-#puts hsh.count
-# hsh.each { |count| puts "Zodis(zodziai) #{word_hsh.select{|k,v| v == count }.
-# keys.join(", ")} pasikartoja #{count} kartus(kartu)"}
 hsh
 end
-
-
 def count_letters(file_name)
   file = File.open(file_name, 'r')
   letters = []
@@ -42,26 +30,16 @@ letters.count
 end
 
 def stats(total_letters, letter_amounts)
-  proc = []
+  hsh2 = Hash.new{0}
   letter_amounts.each do |letters, value|
-    proc << " #{((value.to_f/total_letters)*100 ).round(2)} #{"%"}  #{letters}" 
+      hsh2[letters] = "#{((value.to_f/total_letters)*100 ).round(2)}" 
   end
- # puts proc.sort.reverse
- proc.sort.reverse
+ b = hsh2.sort_by { |k,w| w}.reverse
+ b
 end
-
 file_name = (ARGV[0])
-
 letters_num = read(file_name)
 letter_count = count_letters(file_name)
 file = File.open(ARGV[1], 'w')
-
-#binding.pry
-#puts letter_count
-#stats(letter_count, letters_num)
 file.puts stats(letter_count, letters_num).to_json
-
 file.close
-
-
-
