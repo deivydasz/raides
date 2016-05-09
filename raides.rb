d293007c74cdfcd4bd225c10cbe240fb6057e87b
ecuSinner <deivydas.ziziliauskas@gmail.com>
2016-05-09 19:49:54 +0300

    Accepts arguments, parses to json

--- raides.rb
+++ raides.rb
@@ -1,7 +1,14 @@
-def read()
-  file = File.open('bigtext.txt', 'r')
+#require 'pry'
+require 'json'
+
+def read(file_name)
+  file = File.open(file_name, 'r')
+  text = file.read
+  text = text.chomp.downcase  # split(" ".sort
+   text.gsub!(/\W+/, '')
+   text.gsub!(/\d+/, '')
   hsh = Hash.new(0)
-  file.each_line do |line|
+  text.each_line do |line|
     line.split.each do |word|
       word.split('').each do |letter|
     hsh[letter] += 1
@@ -11,20 +18,18 @@ def read()
 
 end
 
+#puts hsh.count
 
-
-puts hsh.count
-
-puts hsh
-puts hsh.count
+#puts hsh
+#puts hsh.count
 # hsh.each { |count| puts "Zodis(zodziai) #{word_hsh.select{|k,v| v == count }.
 # keys.join(", ")} pasikartoja #{count} kartus(kartu)"}
 hsh
 end
 
 
-def count_letters
-  file = File.open('bigtext.txt', 'r')
+def count_letters(file_name)
+  file = File.open(file_name, 'r')
   letters = []
   file.each_line do |line|
     line.split.each do |word|
@@ -39,18 +44,24 @@ end
 def stats(total_letters, letter_amounts)
   proc = []
   letter_amounts.each do |letters, value|
-    proc << " #{((value.to_f/total_letters) * 100 )}  #{letters}" 
+    proc << " #{((value.to_f/total_letters)*100 ).round(2)} #{"%"}  #{letters}" 
   end
-  puts proc.sort.reverse
+ # puts proc.sort.reverse
+ proc.sort.reverse
 end
 
+file_name = (ARGV[0])
 
-letters_count = read
-letter_count = count_letters
-puts letter_count
+letters_num = read(file_name)
+letter_count = count_letters(file_name)
+file = File.open(ARGV[1], 'w')
 
-stats(letter_count, letters_count)
+#binding.pry
+#puts letter_count
+#stats(letter_count, letters_num)
+file.puts stats(letter_count, letters_num).to_json
 
+file.close
 
 
 
